920b51655bc5642fee582dd4d0053ce0dddc6668
Jacob Lingwall <lingwaja@nga.mil>
2019-02-06 20:41:14 -0600

    first commit

---
+++ testapp.js
@@ -0,0 +1,8 @@
+url = 'https://newsapi.org/v2/everything?q=bitcoin&from=2019-01-07&sortBy=publishedAt&apiKey=0b4ad9ead85c4d52a859b2b801cfa7a7'
+
+console.log('foff')
+d3.json(url, function(data){
+
+    console.log(data)
+})
+console.log('l2')
