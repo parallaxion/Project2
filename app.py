22b48c8a219ca7d16dd763036e7dc312023e6d26
ambientstl <41842065+ambientstl@users.noreply.github.com>
2019-02-14 17:18:53 -0600

    update update.py with translation & keyword extraction

---
+++ app.py
@@ -0,0 +1,39 @@
+import datetime as dt
+import pandas as pd
+import pymongo
+import requests
+
+from config import API_KEY
+#from countries import countries, country_codes
+
+from flask import Flask, jsonify, render_template
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+
+ 
+    return render_template('index.html')
+
+
+
+@app.route("/line")
+def test():   #connect to mongodb
+    conn = 'mongodb://localhost:27017/top_headlines'
+    print(conn)
+    client = pymongo.MongoClient(conn)
+    print(client)
+    # Declare the database
+    db = client["top_headlines"]
+    # Declare the collection
+    collection = db["countries_data"]
+
+    #get data
+    data = collection.find()
+
+    return data
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ translationtest.py
@@ -0,0 +1,2 @@
+from py_translator import Translator
+print(Translator().translate(text='Hello my friend', dest='es').text)
