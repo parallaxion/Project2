596d2ab45e0857f3c1060f417aaa36da5f899eae
Jacob Lingwall <lingwaja@nga.mil>
2019-02-15 23:58:19 -0600

    working map routes

---
+++ static/config.js
@@ -0,0 +1,5 @@
+// API key
+const API_KEY = "pk.eyJ1Ijoia3VsaW5pIiwiYSI6ImNpeWN6bjJ0NjAwcGYzMnJzOWdoNXNqbnEifQ.jEzGgLAwQnZCv9rA6UTfxQ";
+const NEWS_KEY = "d1ec08b1a7784187bf02370654ffdeac" //calm
+//"ff88e39feec44b4db7a1b44f8fe0a617" //ja.lingwall
+//frenziedjustice //"358ac513e47b40fcbc47492633beb76c"
