3e85ef86fe91aeb37651577af7d2c1cc72dc3aeb
ambientstl <41842065+ambientstl@users.noreply.github.com>
2019-02-17 19:46:19 -0600

    add working map with filtered outlines, keyword labels, and keywords popups

--- appRoutes.py
+++ appRoutes.py
@@ -46,12 +46,20 @@ def blob():
     return json.dumps(doubleduece)
 
 
+
+
+
+
+
+
+#route that determines which country's boundaries get drawn
 @app.route("/geojson")
 def geo():
-    #filename = "hello.txt"
+    #open geojson file and load
     with open('countries.geo.json') as f:
         data = json.load(f)
 
+        #formatting to account for Serbia
         formatted_countries= []
         for country in countries:
             if country == "Serbia":
@@ -60,23 +68,93 @@ def geo():
             else:
                 formatted_countries.append(country)
         
+        #set empty list for good geojson data
         goodData = []
+        #for country in data's features,
         for entry in data["features"]:
+            #if country is in our country list,
             if entry["properties"]["name"] in formatted_countries:
+                #add to good data list
                 goodData.append(entry)
-                print(entry["properties"]["name"])
+            #else, skip
             else:
                 pass
-
-
+        #pass good data into geojson format
+        goodData = {'type': 'FeatureCollection',
+            'features': goodData}
+    #dump geojson data into json
     return json.dumps(goodData)
 
-@app.route("/keywords/")
-def keywords():
-    #connect to mongo
-    {{"Country": "Argentina", "Top": ["Trump", "Resigns", "Woman", "Worry", "Markel"]}, {{"Country": "Canada", "Top": ["Hockey", "Kim", "Scared", "Snow", "Oil"]}}}
+#route that returns info to display on map and tooltip
+@app.route("/map_info")
+def map_info():
+    #open center coordinates file and load
+    with open('centroids.json') as centers:
+        data = json.load(centers)
+        #find center coordinates data
+        center_coords = data["center_coords"]
 
-### FILTER BY KEYWORD!!
+        #set collection
+        collection = db["countries_keywords"]
+        #get keywords data
+        data = collection.find()
+
+        #set empty map dictionary
+        mapDict = {"map_info": []}
+        #for country in countries_keywords,
+        for entry in data:
+            #set empty keyword dictionary
+            kw_dict = {"country": "", "coords": {}}
+
+            #set country variable and add to keyword dict
+            country = entry["country"]
+            kw_dict["country"] = country
+            
+            #find center coordinates for country
+            for coords in center_coords:
+                #if country = country, get lat and lon
+                if coords["country"] == country:
+                    lat = coords["coords"][1]
+                    lon = coords["coords"][0]
+                #else, pass
+                else:
+                    pass
+            #add lat and lon to keyword dict
+            kw_dict["coords"]["lat"] = lat
+            kw_dict["coords"]["lon"] = lon
+
+            #get top keyword and add to keyword dict
+            keyword = list(entry["keywords"].keys())[0]
+            kw_dict["keyword"] = keyword
+
+            #append country's keyword dict to map dict
+            mapDict["map_info"].append(kw_dict)
+    return jsonify(mapDict)
+
+#route to return country's top 5 keywords for pop-up
+@app.route("/keywords/<country>")
+def country_keywords(country):
+    #set collection
+    collection = db["countries_keywords"]
+    #find the document that matches the country
+    all_keywords = collection.find_one({"country": country})
+    #list all keywords
+    all_keywords = list(all_keywords["keywords"].keys())
+    #take top five
+    top_five = [all_keywords[i] for i in range(5)]
+    #add top five to keyword dict
+    keyword_dict = {"top_five": top_five}
+    return jsonify (keyword_dict)
+
+@app.route("/country/<country>")
+def country_dash(country):
+    #this is the site that is linked to in the country's tooltip
+    
+    return jsonify ({"To Do": "Country dashboard goes here"})
+
+
+
+
 
 @app.route("/world")
 def test():   #connect to mongodb
